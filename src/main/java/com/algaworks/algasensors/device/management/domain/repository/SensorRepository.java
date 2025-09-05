d5cd322272f6a3d2ea2022ddb4f183af0862246d
Fernanda <fernanda.g.araujo@gmail.com>
2025-09-05 18:30:08 -0300

    Commit de api sensors by id

---
+++ src/main/java/com/algaworks/algasensors/device/management/domain/repository/SensorRepository.java
@@ -0,0 +1,11 @@
+package com.algaworks.algasensors.device.management.domain.repository;
+
+import com.algaworks.algasensors.device.management.domain.model.Sensor;
+import com.algaworks.algasensors.device.management.domain.model.SensorId;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface SensorRepository extends JpaRepository<Sensor, SensorId> {
+
+}
